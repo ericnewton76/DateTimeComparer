b7bb9d128885077a13216534da18be1ad4959efd
Eric Newton <ericnewton76@gmail.com>
2016-12-11 11:19:13 -0500

    initial project files checkin

---
+++ DateTimeComparer/DateTimeComparer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+	public class DateTimeComparer : Comparer<DateTime>
+	{
+		private Precision _Precision;
+
+		public enum Precision : sbyte
+		{
+			Years,
+			Months,
+			Days,
+			Hours,
+			Minutes,
+			Seconds,
+			Milliseconds,
+			Ticks
+		}
+
+		private static Func<DateTime, DateTime, int>[] S_compareFuncs = new Func<DateTime, DateTime, int>[]
+		{
+			(x,y) => { return (x-y) < TimeSpan.FromDays(365) ? 0 : x.CompareTo(y); },
+			(x,y) => { return (x-y) < TimeSpan.FromDays(DateTime.DaysInMonth(x.Year, x.Month)) ? 0 : x.CompareTo(y); },
+			(x,y) => { return (x-y) < TimeSpan.FromDays(1) ? 0 : x.CompareTo(y); },
+			(x,y) => { return (x-y) < TimeSpan.FromHours(1) ? 0 : x.CompareTo(y); },
+			(x,y) => { return (x-y) < TimeSpan.FromMinutes(1) ? 0 : x.CompareTo(y); },
+			(x,y) => { return (x-y) < TimeSpan.FromSeconds(1) ? 0 : x.CompareTo(y); },
+			(x,y) => { return x.Ticks.CompareTo(y.Ticks); },
+		};
+
+		public DateTimeComparer(Precision precision = Precision.Ticks)
+		{
+			_Precision = precision;
+		}
+
+		public override int Compare(DateTime x, DateTime y)
+		{
+			if(_Precision == Precision.Ticks)
+			{
+				return x.Ticks.CompareTo(y.Ticks);
+			}
+
+			for(sbyte i = (sbyte)(_Precision); i >= 0; i--)
+			{
+				int compare = S_compareFuncs[i](x, y);
+				if(compare != 0) return compare;
+			}
+
+			return 0;
+		}
+	}
+}
---
+++ DateTimeComparer_Tests/DateTimeComparer_Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DTC = System.DateTimeComparer;
+
+namespace DateTimeComparer_Tests
+{
+	[TestFixture]
+	public class DateTimeComparer_Tests
+	{
+		[Test]
+		public void WithinSeconds()
+		{
+			var comparer = new DateTimeComparer(DTC.Precision.Seconds);
+
+			DateTime x = DateTime.Now;
+			x = x.AddMilliseconds(x.Millisecond * -1); //clear out milliseconds
+
+			DateTime y = x.AddMilliseconds(100);
+
+			var test = comparer.Compare(x, y);
+
+			Assert.That(test, Is.Zero);
+		}
+	}
+}
