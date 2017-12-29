62bca711c906c94889e7cfe91c3d430279d6ac06
Eric Newton <ericnewton76@gmail.com>
2017-12-29 18:02:41 -0500

    additional tests

--- DateTimeComparer_Tests/DateTimeComparer_Tests.cs
+++ DateTimeComparer_Tests/DateTimeComparer_Tests.cs
@@ -15,15 +15,49 @@ namespace DateTimeComparer_Tests
 		[Test]
 		public void WithinSeconds()
 		{
-			var comparer = new DateTimeComparer(DTC.Precision.Seconds);
-
-			DateTime x = DateTime.Now;
-			x = x.AddMilliseconds(x.Millisecond * -1); //clear out milliseconds
-
+			//arrange
+			DateTime x = new DateTime(2000, 1, 1, 12, 0, 0);
 			DateTime y = x.AddMilliseconds(100);
 
+			//act
+			var comparer = new DateTimeComparer(DTC.Precision.Seconds);
 			var test = comparer.Compare(x, y);
 
+			//assert
+			Assert.That(test, Is.Zero);
+		}
+
+		[Test]
+		public void WithinMinutes()
+		{
+			//arrange
+			DateTime x = new DateTime(2000, 1, 1, 12, 0, 0);
+			x = x.AddSeconds(30); 
+
+			DateTime y = x.AddSeconds(30);
+
+			//act
+			var comparer = new DateTimeComparer(DTC.Precision.Minutes);
+			var test = comparer.Compare(x, y);
+
+			//assert
+			Assert.That(test, Is.Zero);
+		}
+
+		[Test]
+		public void WithinHours()
+		{
+			//arrange
+			DateTime x = new DateTime(2000, 1, 1, 12, 0, 0);
+			x = x.AddMinutes(5);
+
+			DateTime y = x.AddMinutes(30);
+
+			//act
+			var comparer = new DateTimeComparer(DTC.Precision.Hours);
+			var test = comparer.Compare(x, y);
+
+			//assert
 			Assert.That(test, Is.Zero);
 		}
 	}
